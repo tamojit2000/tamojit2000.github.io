dcd3fe9bc479b398599462ec5b8f4384addde6d2
tamojit2000 <48746076+tamojit2000@users.noreply.github.com>
2019-07-12 12:41:21 -0700

    Update second.php

--- second.php
+++ second.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST["submit"]))
 {
-$f=fopen("data.txt","a");
+$f=fopen("tamojit2000.github.io//data.txt","a");
 fwrite($f,$_POST["name"]." \n".$_POST["sms"]." \n");
 fclose($f);
 }
